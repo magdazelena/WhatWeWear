57a70523a7e98c13d4f2987d59574b351630565c
magdazelena <magda.zelezik@gmail.com>
2020-05-21 19:51:50 +0200

    Transparency on video background

--- what-we-wear/src/pages/ExplosionsSequence.js
+++ what-we-wear/src/pages/ExplosionsSequence.js
@@ -1,15 +1,28 @@
 import React, {Component} from 'react';
 import THREE from '../3d/three';
 import SimplexNoise from 'simplex-noise';
+import AxisHelper from '../3d/axis';
 import particlesFragmentShader from '../3d/shaders/particlesFragmentShader';
 import particlesVertexShader from '../3d/shaders/particlesVertexShader';
 class ExplosionsSequence extends Component{
-    canvasRef = React.createRef();
-    clock = new THREE.Clock();
-    simplex = new SimplexNoise();
+    constructor(props){
+        super(props);
+        this.state= {
+            videoRef : null
+        }
+        this.canvasRef = React.createRef();
+        this.clock = new THREE.Clock();
+        this.simplex = new SimplexNoise();
+    }
+    onVideoUpload = node => {
+        this.setState({
+            videoRef : node
+        }, 
+        ()=>this.createVideoTexture()
+        )
+    }
     componentDidMount = () => {
         this.init();
-        this.update();
     }
     init = () => {
         const rand = (min,max) => min + Math.random()*(max-min);
@@ -17,43 +30,48 @@ class ExplosionsSequence extends Component{
         this.color = new THREE.Color();
         //scene
         this.scene = new THREE.Scene();
-        this.texture = this.generateTexture();
+       // this.texture = this.generateTexture();
         //renderer
         this.renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true });
+        this.renderer.setClearColor(0x000000);
+        this.renderer.setSize( window.innerWidth, window.innerHeight )
         document.body.appendChild(this.renderer.domElement);
         //camera
-        this.camera = new THREE.PerspectiveCamera(
-            100,
-            window.innerWidth / window.innerHeight,
-            0.01,
-            10000
-          );
-          this.camera.position.z = 0 
+        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight, 1, 10000);
+          this.camera.position.z = 1000; 
           this.camera.position.x = 0;
-          this.camera.position.y = 0;
+          this.camera.position.y = 100;
+          this.camera.updateProjectionMatrix();
 
-          this.particleGroup = new THREE.Object3D();
-		this.particleGroup.scale.set(10, 10, 10);
+
+       
+
+        this.camera.lookAt(new THREE.Vector3());
           //lights
-        //   let hemiLight = new THREE.HemisphereLight(0xffffff,  0.91);
-        //     hemiLight.position.set(0, 50, 0);
-        //     // Add hemisphere light to scene
-        //     this.scene.add(hemiLight);
+          let hemiLight = new THREE.HemisphereLight(0xffffff,  0.91);
+            hemiLight.position.set(0, 50, 0);
+            // Add hemisphere light to scene
+            this.scene.add(hemiLight);
   
   
             // geometry
             this.geometry = new THREE.BufferGeometry();
-            this.particles = 10;
+            this.particles = 100;
             const radius = 200;
             this.positions = new Float32Array(this.particles * 3);
-			this.colors = new Float32Array(this.particles * 4);
-			this.sizes = new Float32Array(this.particles);
-            this.size = 100;
+			this.colors = new Float32Array(this.particles * 3);
+            this.sizes = new Float32Array(this.particles);
+            this.size = 1000;
             this.parts = [];
-
+            //vec3 attributes
+            this.geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( this.positions, 3 ) );
+            this.geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( this.colors, 3 ) );
+            this.geometry.setAttribute( 'size', new THREE.Float32BufferAttribute( this.sizes, 1 ));
+       
             for ( var i = 0; i < this.particles; i ++ ) {
                 let size = rand(10, 80);
-                this.parts.push({
+                this.color.setHSL( i / this.particles, 1.0, 0.5 );
+                let part = {
                     offset: 0,
                     position: new THREE.Vector3(
                         rand(-this.size / 2, this.size / 2),
@@ -62,27 +80,22 @@ class ExplosionsSequence extends Component{
                     ),
                     baseSize: size,
                     size: size,
-                    r: parseInt(size),
-                    g: parseInt(size),
-                    b: parseInt(size),
+                    r: this.color.r,
+                    g: this.color.g,
+                    b: this.color.b,
                     a: 0.6,
                     life: 2,
                     decay: rand(0.05, 0.15),
                     firstRun: true
-                });
-
+                };
+                this.parts.push(part);
             }
-            //vec3 attributes
-            this.geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( this.positions, 3 ) );
-            this.geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( this.colors, 4 ) );
-            this.geometry.setAttribute( 'size', new THREE.Float32BufferAttribute( this.sizes, 1 ));
-
-
+     
+            
             this.material = new THREE.ShaderMaterial( {
                 uniforms: {
-                    texture: {
-                        value: this.texture
-                    }
+                    pointTexture: { value: new THREE.TextureLoader().load( "../3d/particle.png" ) }
+                    
                 },
                 vertexShader: particlesVertexShader,
                 fragmentShader: particlesFragmentShader,
@@ -92,9 +105,12 @@ class ExplosionsSequence extends Component{
                 vertexColors: true
              });
             this.particleSystem = new THREE.Points(this.geometry, this.material);
-            this.particleGroup.add(this.particleSystem);
-            this.scene.add(this.particleGroup);
+
+            this.scene.add(this.particleSystem);
             this.updateParticleAttributes(true, true, true);
+           
+            window.addEventListener( 'resize', this.onWindowResize, false );
+            this.update();
 
     }
     generateTexture=() =>{
@@ -119,23 +135,26 @@ class ExplosionsSequence extends Component{
 		return texture;
 	}
     updateParticleAttributes=(color, position, size) =>{
-		let i = this.particles;
-		while(i--) {
+		let i = 0;
+		while(i<this.particles) {
 			let part = this.parts[i];
 			if(color) {
-				this.colors[i * 4 + 0] = part.r;
-				this.colors[i * 4 + 1] = part.g;
-				this.colors[i * 4 + 2] = part.b;
-				this.colors[i * 4 + 3] = part.a;
-			}
-			if(position) {
-				this.positions[i * 3 + 0] = part.position.x;
-				this.positions[i * 3 + 1] = part.position.y;
-				this.positions[i * 3 + 2] = part.position.z;
-			}
-			if(size) {
-				this.sizes[i] = part.size;
-            }
+                const colorAttribute = this.geometry.attributes.color;
+                      colorAttribute.array[i * 3 + 0] = part.r;
+                      colorAttribute.array[i * 3 + 1] = part.g;
+                      colorAttribute.array[i * 3 + 2] = part.b;
+                  }
+                  if(position) {
+                const positionAttribute = this.geometry.attributes.position;
+                      positionAttribute.array[i * 3 + 0] = part.position.x;
+                      positionAttribute.array[i * 3 + 1] = part.position.y;
+                      positionAttribute.array[i * 3 + 2] = part.position.z;
+                  }
+                  if(size) {
+                const sizeAttribute = this.geometry.attributes.size;
+                      sizeAttribute.array[i] = part.size;
+             }
+            i++;
 		}
 
 		if(color) {
@@ -146,17 +165,59 @@ class ExplosionsSequence extends Component{
 		}
 		if(size) {
 			this.geometry.attributes.size.needsUpdate = true;
-		}
-	}
+        }
+        this.geometry.computeBoundingSphere();
+    }
+    createVideoTexture = ()=> {
+        const video = this.state.videoRef;
+        video.currentTime = 1;
+        video.width = window.innerWidth;
+        video.height = window.innerHeight;
+       // video.play();
+        video.loop = true;
+        const videoTexture = new THREE.VideoTexture( video );
+        videoTexture.minFilter = THREE.LinearFilter;
+        videoTexture.magFilter = THREE.LinearFilter;
+        videoTexture.format = THREE.RGBFormat;
+    
+        let planeGeo = new THREE.PlaneGeometry(window.innerWidth, window.innerHeight);
+        let planeMaterial = new THREE.ShaderMaterial({
+            uniforms: {
+                texture : {
+                    type: 't',
+                    value: videoTexture
+                },
+                color: {
+                    type: 'c',
+                    value: new THREE.Color(0x000000)
+                }
+            },
+            vertexShader: 'varying vec2 vUv;\
+            void main(){\
+                vUv = uv;\
+                vec4 mvPosition = modelViewMatrix * vec4 (position, 1.0);\
+                gl_Position = projectionMatrix * mvPosition;}',
+            fragmentShader: 'uniform sampler2D texture;\
+            uniform vec3 color;\
+            varying vec2 vUv;\
+            void main(){\
+                vec3 tColor = texture2D( texture, vUv).rgb;\
+                float a = (length(tColor - color) - 0.2) * 0.9;\
+                gl_FragColor = vec4(tColor, a);}',
+            transparent: true
+        })
+        this.plane = new THREE.Mesh(planeGeo, planeMaterial);
+        this.plane.position.x = 0;
+        this.plane.position.y = 0;
+        this.scene.add(this.plane);
+        //this.scene.background = videoTexture;
+    }
     update=()=>{
-        if (this.resizeRendererToDisplaySize(this.renderer)) {
-            const canvas = this.renderer.domElement;
-            this.camera.aspect = canvas.clientWidth / canvas.clientHeight;
-            this.camera.updateProjectionMatrix();
-          }
+        requestAnimationFrame(this.update);
+        const rand = (min,max) => min + Math.random()*(max-min);
           let noiseTime = this.clock.getElapsedTime() * 0.0008;
           let noiseVelocity = this.simplex.noise2D(0,1);
-        const noiseScale = 0.01;
+         const noiseScale = 0.01;
         for ( var i = 0; i < this.particles; i ++ ) {
             let part = this.parts[i];
             let xScaled = part.position.x * noiseScale;
@@ -184,29 +245,44 @@ class ExplosionsSequence extends Component{
             part.position.y += Math.sin(noise2 * Math.PI * 2) * this.clock.getDelta() * noiseVelocity;
             part.position.z += Math.sin(noise3 * Math.PI * 2) * this.clock.getDelta() * noiseVelocity;
             this.parts[i] = part;
+            if(part.life <= 0 || part.firstRun) {
+				part.life = 2;
+				part.position.x = rand(-this.size / 2, this.size / 2);
+				part.position.y = rand(-this.size / 2, this.size / 2);
+				part.position.z = rand(-this.size / 2, this.size / 2);
+
+				
+                let hue = (this.clock.elapsedTime / 25 + rand(90,100)) % 360 + 110;
+                let lightness = Math.round(rand(10, 50));
+                this.color.set(`hsl(${hue}, 85%, ${lightness}%)`);
+
+
+				part.r = parseInt(this.color.r)+100;
+				part.g = parseInt(this.color.g);
+				part.b = parseInt(this.color.b);
+
+                part.firstRun = false;
+            }
+            this.parts[i] = part;
+
         }
-       // this.updateParticleAttributes(true, true, true);
+        this.updateParticleAttributes(true, true, true);
         this.renderer.render(this.scene, this.camera);
-        requestAnimationFrame(this.update);
+        
        
     }
-    resizeRendererToDisplaySize=(renderer)=> {
-        const canvas = renderer.domElement;
-        let width = window.innerWidth;
-        let height = window.innerHeight;
-        let canvasPixelWidth = canvas.width / window.devicePixelRatio;
-        let canvasPixelHeight = canvas.height / window.devicePixelRatio;
-      
-        const needResize =
-          canvasPixelWidth !== width || canvasPixelHeight !== height;
-        if (needResize) {
-          renderer.setSize(width, height, false);
-        }
-        return needResize;
-      }
+    onWindowResize=()=> {
+
+        this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
+
+    }
     render=()=>{
         return <div>
             <canvas ref={ref=>{this.canvasRef = ref}}></canvas>
+            <video src="../images/color_web1_.mp4" id="video" ref={this.onVideoUpload}></video>
         </div>
     }
 }
