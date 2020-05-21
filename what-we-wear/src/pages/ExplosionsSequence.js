b782f9ad24fad9723b24a7d1d51b14aa30e9c414
magdazelena <magda.zelezik@gmail.com>
2020-05-21 21:28:28 +0200

    WIP - particles

--- what-we-wear/src/pages/ExplosionsSequence.js
+++ what-we-wear/src/pages/ExplosionsSequence.js
@@ -18,7 +18,7 @@ class ExplosionsSequence extends Component{
         this.setState({
             videoRef : node
         }, 
-        ()=>this.createVideoTexture()
+        //()=>this.createVideoTexture()
         )
     }
     componentDidMount = () => {
@@ -30,10 +30,8 @@ class ExplosionsSequence extends Component{
         this.color = new THREE.Color();
         //scene
         this.scene = new THREE.Scene();
-       // this.texture = this.generateTexture();
         //renderer
         this.renderer = new THREE.WebGLRenderer({ canvas, antialias: true, alpha: true });
-        this.renderer.setClearColor(0x000000);
         this.renderer.setSize( window.innerWidth, window.innerHeight )
         document.body.appendChild(this.renderer.domElement);
         //camera
@@ -56,16 +54,16 @@ class ExplosionsSequence extends Component{
   
             // geometry
             this.geometry = new THREE.BufferGeometry();
-            this.particles = 100;
+            this.particles = 10000;
             const radius = 200;
             this.positions = new Float32Array(this.particles * 3);
-			this.colors = new Float32Array(this.particles * 3);
+			this.colors = new Float32Array(this.particles * 4);
             this.sizes = new Float32Array(this.particles);
             this.size = 1000;
             this.parts = [];
             //vec3 attributes
             this.geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( this.positions, 3 ) );
-            this.geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( this.colors, 3 ) );
+            this.geometry.setAttribute( 'color', new THREE.Float32BufferAttribute( this.colors, 4 ) );
             this.geometry.setAttribute( 'size', new THREE.Float32BufferAttribute( this.sizes, 1 ));
        
             for ( var i = 0; i < this.particles; i ++ ) {
@@ -113,27 +111,7 @@ class ExplosionsSequence extends Component{
             this.update();
 
     }
-    generateTexture=() =>{
-		let c = document.createElement('canvas');
-		let ctx = c.getContext('2d');
-		let size = 256;
-		c.width = size;
-		c.height = size;
 
-		let gradient = ctx.createRadialGradient(size * 0.5, size * 0.5, 0, size * 0.5, size * 0.5, size * 0.4);
-		gradient.addColorStop(0, 'hsla(100, 80%, 60%, 1)');
-		gradient.addColorStop(1, 'hsla(100, 80%, 60%, 0.6 )');
-
-		ctx.beginPath();
-		ctx.arc(size / 2, size / 2, size / 2, 0, Math.PI * 2);
-		ctx.fillStyle = gradient;
-		ctx.fill();
-
-        let texture = new THREE.Texture(c);
-		texture.needsUpdate = true;
-
-		return texture;
-	}
     updateParticleAttributes=(color, position, size) =>{
 		let i = 0;
 		while(i<this.particles) {
@@ -143,6 +121,7 @@ class ExplosionsSequence extends Component{
                       colorAttribute.array[i * 3 + 0] = part.r;
                       colorAttribute.array[i * 3 + 1] = part.g;
                       colorAttribute.array[i * 3 + 2] = part.b;
+                      colorAttribute.array[i * 4 + 3] = part.a;
                   }
                   if(position) {
                 const positionAttribute = this.geometry.attributes.position;
@@ -173,7 +152,8 @@ class ExplosionsSequence extends Component{
         video.currentTime = 1;
         video.width = window.innerWidth;
         video.height = window.innerHeight;
-       // video.play();
+        video.mute = true;
+        video.play();
         video.loop = true;
         const videoTexture = new THREE.VideoTexture( video );
         videoTexture.minFilter = THREE.LinearFilter;
@@ -216,8 +196,9 @@ class ExplosionsSequence extends Component{
         requestAnimationFrame(this.update);
         const rand = (min,max) => min + Math.random()*(max-min);
           let noiseTime = this.clock.getElapsedTime() * 0.0008;
-          let noiseVelocity = this.simplex.noise2D(0,1);
-         const noiseScale = 0.01;
+          let noiseVelocity = rand(280,80);
+         const noiseScale = 0.001;
+         const delta = this.clock.getDelta();
         for ( var i = 0; i < this.particles; i ++ ) {
             let part = this.parts[i];
             let xScaled = part.position.x * noiseScale;
@@ -241,10 +222,21 @@ class ExplosionsSequence extends Component{
 				zScaled+200,
 				50 + noiseTime
             )* 0.5 + 0.5;
-            part.position.x += Math.sin(noise1 * Math.PI * 2) * this.clock.getDelta() * noiseVelocity;
-            part.position.y += Math.sin(noise2 * Math.PI * 2) * this.clock.getDelta() * noiseVelocity;
-            part.position.z += Math.sin(noise3 * Math.PI * 2) * this.clock.getDelta() * noiseVelocity;
-            this.parts[i] = part;
+            part.position.x -= Math.sin(noise1 * Math.PI * 2) - delta * noiseVelocity;
+            part.position.y -= Math.sin(noise2 * Math.PI * 2) - delta * noiseVelocity;
+            part.position.z -= Math.sin(noise3 * Math.PI * 2) - delta * noiseVelocity;
+            if(part.position.x < -window.innerWidth/2-20)
+                part.position.x = window.innerWidth;
+            if(part.position.y < -window.innerHeight/2-20)
+                part.position.y = window.innerHeight;
+            if(part.position.x > window.innerWidth/2+20)
+                part.position.x = 0;
+            if(part.position.y > window.innerHeight/2+20)
+                part.position.y = 0;
+
+            if(part.life > 0 ) {
+				part.life -= part.decay * delta;
+			}
             if(part.life <= 0 || part.firstRun) {
 				part.life = 2;
 				part.position.x = rand(-this.size / 2, this.size / 2);
@@ -252,12 +244,12 @@ class ExplosionsSequence extends Component{
 				part.position.z = rand(-this.size / 2, this.size / 2);
 
 				
-                let hue = (this.clock.elapsedTime / 25 + rand(90,100)) % 360 + 110;
-                let lightness = Math.round(rand(10, 50));
-                this.color.set(`hsl(${hue}, 85%, ${lightness}%)`);
+                let hue = (this.clock.elapsedTime / 25 + rand(190,200)) % 360 + 110;
+                let lightness = Math.round(rand(90, 100));
+                this.color.set(`hsl(${hue}, 95%, 100%)`);
 
 
-				part.r = parseInt(this.color.r)+100;
+				part.r = parseInt(this.color.r);
 				part.g = parseInt(this.color.g);
 				part.b = parseInt(this.color.b);
 
