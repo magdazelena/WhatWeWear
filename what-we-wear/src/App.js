7e7f0ee8146837740ab947d5460a83170cd1c67b
magdazelena <magda.zelezik@gmail.com>
2020-06-18 13:45:21 +0200

    Texts for substance sequence

--- what-we-wear/src/App.js
+++ what-we-wear/src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     super(props);
     this.state ={
       introIsDone: true,
-      sceneId:4
+      sceneId:5
     }
     this.controller = new ScrollMagic.Controller();
     this.markIntroDone = this.markIntroDone.bind(this);
--- what-we-wear/src/pages/substanceSequence.js
+++ what-we-wear/src/pages/substanceSequence.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
 import THREE from '../3d/three';
 import ScrollMagic from 'scrollmagic';
-import {TweenLite} from 'gsap';
+import {TimelineMax, TweenLite} from 'gsap';
+import texts from '../dictionary/en.json';
+import {animateText,  generateTextForAnimation} from '../helpers/textAnimations';
 import ScrollDown from '../objects/ScrollDown';
 class SubstanceSequence extends Component{
     constructor(props){
@@ -11,6 +13,8 @@ class SubstanceSequence extends Component{
             sequenceRef: null,
             shouldAnimate: false,
             shouldAnimateDesc: false, 
+            shouldAnimateDesc2: false,
+            shouldAnimateDesc3: false,  
             shouldAnimateSeason: false,
             counter: 1
         }
@@ -19,7 +23,10 @@ class SubstanceSequence extends Component{
         this.headlineRef = React.createRef();
         this.numberRef = React.createRef();
         this.pRef = React.createRef();
-        this.descriptionRef = React.createRef();
+        this.descRef = React.createRef();
+        this.desc2Ref = React.createRef();
+        this.desc3Ref = React.createRef();
+        this.tl = new TimelineMax();
     }
     onVideoUpload = node => {
         this.setState({
@@ -40,13 +47,60 @@ class SubstanceSequence extends Component{
         }
         )
     }
-
+    animateTexts = function(){
+        this.tl.to('canvas', .5, {
+            opacity: .4
+        })
+        this.tl.to(this.headlineRef, .3, {
+            onComplete: ()=> {
+                this.setState({
+                    shouldAnimate: true
+                }, ()=>{
+                    [...this.headlineRef.getElementsByTagName('span')].forEach((span, i)=>{
+                        animateText(span, i).play();
+                    });
+                })
+            }
+        })
+        this.tl.to(this.descRef, 1, {
+            onComplete: ()=> {
+                this.setState({
+                    shouldAnimateDesc: true
+                }, ()=>{
+                    [...this.descRef.getElementsByTagName('span')].forEach((span, i)=>{
+                        animateText(span, i).play();
+                    });
+                })
+            }
+        })
+        this.tl.to(this.desc2Ref, 1, {
+            onComplete: ()=> {
+                this.setState({
+                    shouldAnimateDesc2: true
+                }, ()=>{
+                    [...this.desc2Ref.getElementsByTagName('span')].forEach((span, i)=>{
+                        animateText(span, i).play();
+                    });
+                })
+            }
+        })
+        this.tl.to(this.desc3Ref, 1, {
+            onComplete: ()=> {
+                this.setState({
+                    shouldAnimateDesc3: true
+                }, ()=>{
+                    [...this.desc3Ref.getElementsByTagName('span')].forEach((span, i)=>{
+                        animateText(span, i).play();
+                    });
+                })
+            }
+        })
+    }
     onScroll = ()=>{
             let scene = new ScrollMagic.Scene({
               duration: "60%",
               triggerElement: this.state.sequenceRef
             })
-            .addIndicators()
             .on('leave', e=>{
                 if(e.scrollDirection === "FORWARD"){
                     this.props.nextScene();
@@ -96,6 +150,9 @@ class SubstanceSequence extends Component{
             TweenLite.to(this.buttonRef, 1, {
                 opacity: 1
             })
+            this.animateTexts();
+            video.loop = true;
+            video.play();
         })
         const videoTexture = new THREE.VideoTexture( video );
         videoTexture.minFilter = THREE.LinearFilter;
@@ -139,10 +196,22 @@ class SubstanceSequence extends Component{
 
     }
     render=()=>{
-        return <div >
-                <div ref={this.onSequenceLoad}> 
+        return <div>
+                <div id="substanceSection" ref={this.onSequenceLoad}> 
                     <canvas ref={ref=>{this.canvasRef = ref}}></canvas>
-                    <video src="../images/bawelna.mp4" id="video" ref={this.onVideoUpload}></video>
+                    <video src="../images/bawelna23.mp4" id="video" ref={this.onVideoUpload}></video>
+                    <div id="substanceHeadline" ref={ref=>{this.headlineRef = ref}}>
+                        {this.state.shouldAnimate && (generateTextForAnimation(texts.substanceSequence.headline.split('')))}
+                    </div>
+                    <div id="substanceDesc" ref={ref=>{this.descRef = ref}}>
+                        {this.state.shouldAnimateDesc && (generateTextForAnimation(texts.substanceSequence.description.split('')))}
+                    </div>
+                    <div id="substanceDesc2" ref={ref=>{this.desc2Ref = ref}}>
+                        {this.state.shouldAnimateDesc2 && (generateTextForAnimation(texts.substanceSequence.description2.split('')))}
+                    </div>
+                    <div id="substanceDesc3" ref={ref=>{this.desc3Ref = ref}}>
+                        {this.state.shouldAnimateDesc3 && (generateTextForAnimation(texts.substanceSequence.description3.split('')))}
+                    </div>
                     <div ref={ref=>this.buttonRef = ref} className="show-up">
                 <ScrollDown  />
             </div>
