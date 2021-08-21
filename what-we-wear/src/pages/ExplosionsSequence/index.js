852e513e4f3e66e3fbfae7c5da223300402f2b4e
magdazelena <magda.zelezik@gmail.com>
2021-08-21 21:33:08 +0200

    Refactor explosions to hooks, remove scroll

--- what-we-wear/src/pages/ExplosionsSequence/index.js
+++ what-we-wear/src/pages/ExplosionsSequence/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useRef, useState } from 'react';
 import THREE from '3d/three';
 import NextButton from 'objects/NextButton';
 import ScrollMagic from 'scrollmagic';
@@ -10,76 +10,57 @@ import AnimatedText, { animateComponentText } from '../components/AnimatedText';
 import gsap from 'gsap';
 
 //particle system by example of Rugile, Jack: https://github.com/jackrugile/3d-particle-explorations/blob/master/js/demo-8/system.js
-class ExplosionsSequence extends Component {
-	constructor(props) {
-		super();
-		this.state = {
-			videoRef: null,
-			sequenceRef: null,
-			shouldAnimate: false,
-			shouldAnimateDesc: false,
-			shouldAnimateSeason: false,
-			counter: 1
+const ExplosionsSequence = (props) => {
+	const { renderer, onUnmount, nextScene } = props
+	const videoRef = useRef()
+	const sequenceRef = useRef()
+	const headlineRef = useRef()
+	const descriptionRef = useRef()
+	const buttonRef = useRef()
+	const numberRef = useRef()
+	const [shouldAnimate, setShouldAnimate] = useState(false)
+	const [shouldAnimateDesc, setShouldAnimateDesc] = useState(false)
+	const [shouldAnimateSeason, setShouldAnimateSeason] = useState(false)
+	const [counter, setCounter] = useState(1)
+
+	const scene = new THREE.Scene();
+	document.body.classList.add('fixed');
+
+	useEffect(() => {
+		return () => {
+			document.body.classList.remove('fixed')
+			onUnmount()
 		}
-		this.renderer = props.renderer;
-		this.camera = camera;
-		this.references = {
-			headlineRef: React.createRef(),
-			numberRef: React.createRef(),
-			descriptionRef: React.createRef(),
-			buttonRef: React.createRef(),
+	}, [])
+
+	useEffect(() => {
+		if (videoRef) animateInfo()
+	}, [videoRef])
+
+	useEffect(() => {
+		if (sequenceRef) {
+			init();
+			createVideoTexture()
 		}
-		this.scene = new THREE.Scene();
-		document.body.classList.add('fixed');
-	}
-	componentDidMount = () => {
-		this._isMounted = true;
-	}
-	componentWillUnmount = () => {
-		this._isMounted = false;
-		this.references = {};
-		this.props.onUnmount();
-		document.body.classList.remove('fixed');
-	}
-	onVideoUpload = node => {
-		this.setState({
-			videoRef: node
-		},
-			() => {
+	}, [sequenceRef])
+	useEffect(() => {
+		if (shouldAnimate) animateComponentText(headlineRef.current)
+	}, [shouldAnimate])
 
-				this.animateInfo();
+	useEffect(() => {
+		if (shouldAnimateDesc) animateComponentText(descriptionRef.current)
+	}, [shouldAnimateDesc])
 
-			}
-		)
-	}
-	onSequenceLoad = node => {
-		this.setState({
-			sequenceRef: node
-		},
-			() => {
-				if (this._isMounted) {
-					this.init();
-					this.createVideoTexture();
-					this.onScroll();
-				}
-			}
-		)
-	}
-
-	animateInfo = () => {
-		let counter = { value: this.state.counter };
+	const animateInfo = () => {
+		let animCounter = { value: counter };
 		let timeline = gsap.timeline();
-		timeline.to(this.references.headlineRef.current, {
+		timeline.to(headlineRef.current, {
 			duration: 0.2,
 			onComplete: () => {
-				this.setState({
-					shouldAnimate: true
-				}, () => {
-					animateComponentText(this.references.headlineRef.current);
-				})
+				setShouldAnimate(true)
 			},
 		});
-		timeline.to(this.references.numberRef, {
+		timeline.to(numberRef.current, {
 			duration: 3,
 			opacity: 1
 		}, '+=2');
@@ -87,70 +68,47 @@ class ExplosionsSequence extends Component {
 			duration: 3,
 			value: 26,
 			roundProps: 'value',
-			onUpdate: () => updateCounter(counter.value)
+			onUpdate: () => updateCounter(animCounter.value)
 		}, "-=3");
-		timeline.to(this.references.descriptionRef.current, {
+		timeline.to(descriptionRef.current, {
 			duration: 0.2,
 			onComplete: () => {
-				this.setState({
-					shouldAnimateDesc: true
-				}, () => {
-					animateComponentText(this.references.descriptionRef.current)
-				})
+				setShouldAnimateDesc(true)
 			},
 		}, "-=2");
-		timeline.to(this.references.buttonRef, {
+		timeline.to(buttonRef.current, {
 			duration: 1,
 			opacity: 1
 		})
 		let updateCounter = value => {
-			this.setState({
-				counter: value
-			})
+			setCounter(value)
 		}
 
 	}
-	onScroll = () => {
-		let scene = new ScrollMagic.Scene({
-			duration: "70%",
-			offset: 500,
-			triggerElement: this.state.sequenceRef
-		})
-			.on('enter', () => {
-				this.video.play();
-			})
-			.on('leave', e => {
-				this.video.pause();
-				scene.remove();
-			})
-			.addTo(this.props.controller);
-	}
 
-	init = () => {
-		console.log(this.renderer);
-		this.controls = new THREE.OrbitControls(this.camera, this.renderer.domElement);
-		this.controls.maxDistance = 1000;
-		this.controls.minDistance = 2;
-		this.camera.position.set(0, 20, 100);
-		this.controls.update();
-		this.camera.near = 1;
-		this.camera.far = 10000;
-		this.camera.position.z = 1000;
-		this.camera.position.x = 0;
-		this.camera.position.y = 100;
-		this.camera.updateProjectionMatrix();
-		this.camera.lookAt(new THREE.Vector3());
+	const init = () => {
+		const controls = new THREE.OrbitControls(camera, renderer.domElement);
+		controls.maxDistance = 1000;
+		controls.minDistance = 2;
+		camera.position.set(0, 20, 100);
+		controls.update();
+		camera.near = 1;
+		camera.far = 10000;
+		camera.position.z = 1000;
+		camera.position.x = 0;
+		camera.position.y = 100;
+		camera.updateProjectionMatrix();
+		camera.lookAt(new THREE.Vector3());
 		yellowHemiLight.position.set(0, 50, 0);
-		this.scene.add(yellowHemiLight);
-		window.addEventListener('resize', this.onWindowResize, false);
+		scene.add(yellowHemiLight);
+		window.addEventListener('resize', onWindowResize, false);
 	}
 
-	createVideoTexture = () => {
-		const video = this.state.videoRef;
+	const createVideoTexture = () => {
+		const video = videoRef.current;
 		video.currentTime = 1;
 		video.muted = true;
 		video.loop = true;
-		this.video = video;
 		const videoTexture = new THREE.VideoTexture(video);
 		videoTexture.minFilter = THREE.LinearFilter;
 		videoTexture.magFilter = THREE.LinearFilter;
@@ -168,56 +126,55 @@ class ExplosionsSequence extends Component {
 				}
 			},
 			vertexShader: 'varying vec2 vUv;void main(){vUv = uv;vec4 mvPosition = modelViewMatrix * vec4 (position, 1.0);gl_Position = projectionMatrix * mvPosition;}',
-			fragmentShader: 'uniform sampler2D texture;uniform vec3 color;varying vec2 vUv;void main(){vec3 tColor = texture2D( texture, vUv).rgb;float a = (length(tColor - color) - 0.1) * 0.9;gl_FragColor = vec4(tColor, a);}',
+			fragmentShader: 'uniform sampler2D texture;uniform vec3 color;varying vec2 vUv;void main(){vec3 tColor = texture2D( texture, vUv).rgb;float a = (length(tColor - color) - 0.7) * 0.9;gl_FragColor = vec4(tColor, a);}',
 			transparent: true
 		})
-		this.plane = new THREE.Mesh(planeGeo, planeMaterial);
-		this.plane.position.x = 0;
-		this.plane.position.y = 0;
-		this.scene.add(this.plane);
+		const plane = new THREE.Mesh(planeGeo, planeMaterial);
+		plane.position.x = 0;
+		plane.position.y = 0;
+		scene.add(plane);
 	}
-	update = () => {
-		if (!this._isMounted) return;
-		requestAnimationFrame(this.update);
+	const update = () => {
+		requestAnimationFrame(update);
 
-		this.renderer.render(this.scene, this.camera);
+		renderer.render(scene, camera);
 	}
-	onWindowResize = () => {
-		this.camera.aspect = window.innerWidth / window.innerHeight;
-		this.camera.updateProjectionMatrix();
-		this.renderer.setSize(window.innerWidth, window.innerHeight);
+	const onWindowResize = () => {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize(window.innerWidth, window.innerHeight);
 	}
-	render = () => {
-		return <div id="explosionsContainer">
-			<div id="explosionsSequence" ref={this.onSequenceLoad}>
-				<video src="images/Untitled.mp4" muted="muted" autoPlay={true} id="explosionsVideo" ref={this.onVideoUpload}></video>
-				<AnimatedText
-					id="explosionsHeadline"
-					ref={this.references.headlineRef}
-					animatedText={
-						[{
-							shouldAnimate: this.state.shouldAnimate,
-							text: texts.explosionsSequence.headline
-						}]
-					}
-				/>
-				<div id="explosionsNumber" ref={ref => { this.references.numberRef = ref }}>{this.state.counter}</div>
-				<AnimatedText
-					id="explosionsDescription"
-					ref={this.references.descriptionRef}
-					animatedText={
-						[{
-							shouldAnimate: this.state.shouldAnimateDesc,
-							text: texts.explosionsSequence.description
-						}]
-					}
-				/>
-				<div ref={ref => this.references.buttonRef = ref} className="show-up">
-					<NextButton onClick={this.props.nextScene} />
-				</div>
+
+	return <div id="explosionsContainer">
+		<div id="explosionsSequence" ref={sequenceRef}>
+			<video src="images/Untitled.mp4" muted="muted" autoPlay={true} id="explosionsVideo" ref={videoRef}></video>
+			<AnimatedText
+				id="explosionsHeadline"
+				ref={headlineRef}
+				animatedText={
+					[{
+						shouldAnimate,
+						text: texts.explosionsSequence.headline
+					}]
+				}
+			/>
+			<div id="explosionsNumber" ref={numberRef}>{counter}</div>
+			<AnimatedText
+				id="explosionsDescription"
+				ref={descriptionRef}
+				animatedText={
+					[{
+						shouldAnimate: shouldAnimateDesc,
+						text: texts.explosionsSequence.description
+					}]
+				}
+			/>
+			<div ref={buttonRef} className="show-up">
+				<NextButton onClick={nextScene} />
 			</div>
 		</div>
-	}
+	</div>
+
 }
 
 export default ExplosionsSequence;
