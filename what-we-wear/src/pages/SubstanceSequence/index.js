243e867228550895b3268918498ea7acabe36bdd
magdazelena <magda.zelezik@gmail.com>
2021-08-22 17:29:28 +0200

    Refactored subtance sequence

--- what-we-wear/src/pages/SubstanceSequence/index.js
+++ what-we-wear/src/pages/SubstanceSequence/index.js
@@ -1,160 +1,124 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useRef, useState } from 'react';
 import ScrollMagic from 'scrollmagic';
 import { TimelineMax, TweenLite } from 'gsap';
 import texts from 'dictionary/en.json';
 import { animateText, generateTextForAnimation } from 'helpers/textAnimations';
 import ScrollDown from 'objects/ScrollDown';
-class SubstanceSequence extends Component {
-	constructor(props) {
-		super();
-		this.state = {
-			videoRef: null,
-			sequenceRef: null,
-			shouldAnimate: false,
-			shouldAnimateDesc: false,
-			shouldAnimateDesc2: false,
-			shouldAnimateDesc3: false,
-			shouldAnimateSeason: false,
-			counter: 1
+import NextButton from 'objects/NextButton';
+const SubstanceSequence = (props) => {
+	const { renderer, nextScene, onUnmount } = props
+	let refs = {
+		buttonRef: useRef(),
+		headlineRef: useRef(),
+		descRef: useRef(),
+		desc2Ref: useRef(),
+		desc3Ref: useRef(),
+		videoRef: useRef(),
+		sequenceRef: useRef()
+	}
+	const [shouldAnimate, setShouldAnimate] = useState(false)
+	const [shouldAnimateDesc, setShouldAnimateDesc] = useState(false)
+	const [shouldAnimateDesc2, setShouldAnimateDesc2] = useState(false)
+	const [shouldAnimateDesc3, setShouldAnimateDesc3] = useState(false)
+	const tl = new TimelineMax()
+	const [counter, setCounter] = useState(1)
+
+	useEffect(() => {
+		createVideoTexture()
+		return () => {
+			refs = {}
+			onUnmount()
 		}
-	}
-	componentDidMount = () => {
-		this.buttonRef = React.createRef();
-		this.headlineRef = React.createRef();
-		this.descRef = React.createRef();
-		this.desc2Ref = React.createRef();
-		this.desc3Ref = React.createRef();
-		this.tl = new TimelineMax();
-		this._isMounted = true;
-	}
-	componentWillUnmount = () => {
-		this._isMounted = false;
-	}
-	onVideoUpload = node => {
-		this.setState({
-			videoRef: node
-		},
-			() => {
-				this.onScroll();
-			}
-		)
-	}
-	onSequenceLoad = node => {
-		this.setState({
-			sequenceRef: node
-		},
-			() => {
-				this.createVideoTexture();
-			}
-		)
-	}
-	animateTexts = function () {
-		this.tl.to('video', .5, {
+	})
+	useEffect(() => {
+		if (shouldAnimate) [...refs.headlineRef.current.getElementsByTagName('span')].forEach((span, i) => {
+			animateText(span, i).play();
+		})
+	}, [shouldAnimate])
+
+	useEffect(() => {
+		if (shouldAnimateDesc) [...refs.descRef.current.getElementsByTagName('span')].forEach((span, i) => {
+			animateText(span, i).play();
+		})
+	}, [shouldAnimateDesc])
+	useEffect(() => {
+		if (shouldAnimateDesc2) [...refs.desc2Ref.current.getElementsByTagName('span')].forEach((span, i) => {
+			animateText(span, i).play();
+		})
+	}, [shouldAnimateDesc2])
+	useEffect(() => {
+		if (shouldAnimateDesc3) [...refs.desc3Ref.current.getElementsByTagName('span')].forEach((span, i) => {
+			animateText(span, i).play();
+		})
+	}, [shouldAnimateDesc3])
+	const animateTexts = function () {
+		tl.to('video', {
+			duration: .5,
 			opacity: .2
 		})
-		this.tl.to(this.headlineRef, .3, {
+		tl.to(refs.headlineRef.current, {
+			duration: .3,
 			onComplete: () => {
-				this.setState({
-					shouldAnimate: true
-				}, () => {
-					[...this.headlineRef.getElementsByTagName('span')].forEach((span, i) => {
-						animateText(span, i).play();
-					});
-				})
+				setShouldAnimate(true)
 			}
 		})
-		this.tl.to(this.descRef, 1, {
+		tl.to(refs.descRef.current, {
+			duration: 1,
 			onComplete: () => {
-				this.setState({
-					shouldAnimateDesc: true
-				}, () => {
-					[...this.descRef.getElementsByTagName('span')].forEach((span, i) => {
-						animateText(span, i).play();
-					});
-				})
+				setShouldAnimateDesc(true)
 			}
 		})
-		this.tl.to(this.desc2Ref, 1, {
+		tl.to(refs.desc2Ref.current, {
+			duration: 1,
 			onComplete: () => {
-				this.setState({
-					shouldAnimateDesc2: true
-				}, () => {
-					[...this.desc2Ref.getElementsByTagName('span')].forEach((span, i) => {
-						animateText(span, i).play();
-					});
-				})
+				setShouldAnimateDesc2(true)
 			}
 		})
-		this.tl.to(this.desc3Ref, 1, {
+		tl.to(refs.desc3Ref.current, {
+			duration: 1,
 			onComplete: () => {
-				this.setState({
-					shouldAnimateDesc3: true
-				}, () => {
-					[...this.desc3Ref.getElementsByTagName('span')].forEach((span, i) => {
-						animateText(span, i).play();
-					});
-				})
+				setShouldAnimateDesc3(true)
 			}
 		})
 	}
-	onScroll = () => {
-		let scene = new ScrollMagic.Scene({
-			duration: "60%",
-			triggerElement: this.state.sequenceRef
-		})
-			.on('leave', e => {
-				if (e.scrollDirection === "FORWARD") {
-					this.props.nextScene();
 
-				} else {
-					this.props.prevScene();
-				}
-				scene.remove();
-
-			})
-			.addTo(this.props.controller);
-	}
-
-
-	createVideoTexture = () => {
-		const video = this.state.videoRef;
+	const createVideoTexture = () => {
+		const video = refs.videoRef.current;
 		if (!video) return;
 		video.currentTime = 1;
 		video.mute = true;
-		this.video = video;
-		this.video.play();
-		this.video.addEventListener('ended', () => {
-			TweenLite.to(this.buttonRef, 1, {
+		video.play();
+		video.addEventListener('ended', () => {
+			TweenLite.to(refs.buttonRef.current, 1, {
 				opacity: 1
 			})
-			this.animateTexts();
+			animateTexts();
 			video.loop = true;
 			video.play();
 		})
 
 	}
 
-	render = () => {
-		return <div>
-			<div id="substanceSection" ref={this.onSequenceLoad}>
-				<video src="images/cotton3_.mp4" id="video" ref={this.onVideoUpload}></video>
-				<div id="substanceHeadline" ref={ref => { this.headlineRef = ref }}>
-					{this.state.shouldAnimate && (generateTextForAnimation(texts.substanceSequence.headline.split('')))}
-				</div>
-				<div id="substanceDesc" ref={ref => { this.descRef = ref }}>
-					{this.state.shouldAnimateDesc && (generateTextForAnimation(texts.substanceSequence.description.split('')))}
-				</div>
-				<div id="substanceDesc2" ref={ref => { this.desc2Ref = ref }}>
-					{this.state.shouldAnimateDesc2 && (generateTextForAnimation(texts.substanceSequence.description2.split('')))}
-				</div>
-				<div id="substanceDesc3" ref={ref => { this.desc3Ref = ref }}>
-					{this.state.shouldAnimateDesc3 && (generateTextForAnimation(texts.substanceSequence.description3.split('')))}
-				</div>
-				<div ref={ref => this.buttonRef = ref} className="show-up">
-					<ScrollDown />
-				</div>
+	return <div>
+		<div id="substanceSection" ref={refs.sequenceRef}>
+			<video src="images/cotton3_.mp4" id="video" ref={refs.videoRef}></video>
+			<div id="substanceHeadline" ref={refs.headlineRef}>
+				{shouldAnimate && (generateTextForAnimation(texts.substanceSequence.headline.split('')))}
+			</div>
+			<div id="substanceDesc" ref={refs.descRef}>
+				{shouldAnimateDesc && (generateTextForAnimation(texts.substanceSequence.description.split('')))}
+			</div>
+			<div id="substanceDesc2" ref={refs.desc2Ref}>
+				{shouldAnimateDesc2 && (generateTextForAnimation(texts.substanceSequence.description2.split('')))}
+			</div>
+			<div id="substanceDesc3" ref={refs.desc3Ref}>
+				{shouldAnimateDesc3 && (generateTextForAnimation(texts.substanceSequence.description3.split('')))}
+			</div>
+			<div ref={refs.buttonRef} className="show-up">
+				<NextButton onClick={nextScene} />
 			</div>
 		</div>
-	}
+	</div>
+
 }
 export default SubstanceSequence;
