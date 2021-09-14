38148be1e0990c4354a6986bc0cb1975e9810bf7
magdazelena <magda.zelezik@gmail.com>
2021-09-14 21:22:35 +0200

    Added text animation time getter

--- what-we-wear/src/pages/components/textElement.js
+++ what-we-wear/src/pages/components/textElement.js
@@ -1,5 +1,7 @@
 import React from 'react'
+const time = 0.1
 const TextElement = ({ text, backwards }) => {
-  return <span className='type' style={{ '--n': text.length }}>{text}</span>
+  return <span className='type' style={{ '--n': text.length, '--time': `${time}s` }}>{text}</span>
 }
+export const getTextTimeout = (text) => text.length * time
 export default TextElement
